9b944a738d45626b39117b24b516c475054e0a50
Victor Gonzalez <dazedbutamused@gmail.com>
2020-07-04 14:12:59 -0700

    more tweaks

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+
+const createAccount = () => {
+
+};
 class Admin extends React.Component {
   state = {};
   render() {
     return <div>Admin Content Page</div>;
   }
-}
+};
 
 export default Admin;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 class SignUp extends React.Component {
   state = {};
 
