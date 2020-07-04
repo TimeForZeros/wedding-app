4a338b704d4d09024f3654e1aeba16eb9bbd6e76
Victor Gonzalez <dazedbutamused@gmail.com>
2020-07-03 23:53:45 -0700

    update

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './pages/dashboard';
+import logo from './logo.svg';
+import './App.css';
+/* eslint-disable react/jsx-filename-extension */
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
