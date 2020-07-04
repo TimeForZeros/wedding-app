80c9a3f587bc4f8f7850a9eac51f53b73f72b9e5
Victor Gonzalez <dazedbutamused@gmail.com>
2020-07-04 02:07:53 -0700

    lint edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Dashboard from './pages/dashboard';
-import Signup from './pages/signup';
 import logo from './logo.svg';
+import Dashboard from './pages/Dashboard';
+import SignUp from './pages/SignUp';
 import './App.css';
-/* eslint-disable react/jsx-filename-extension */
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Signup />
+      <Dashboard/>
+      <SignUp/>
     </div>
   );
 }
