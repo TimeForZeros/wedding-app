1a684d09bb8916f68b267bec30057a2fc1803080
Victor Gonzalez <dazedbutamused@gmail.com>
2020-07-04 14:30:30 -0700

    created guestList schema

---
+++ server/api/models/guestList.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const guestListSchema = new mongoose.Schema({
+  guests: [
+    {
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+    },
+  ],
+  notes: { type: String },
+  timestamp: true,
+});
+
+module.exports = mongoose.model('GuestList', guestListSchema);
