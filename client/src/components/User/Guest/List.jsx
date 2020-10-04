79b277579f5e51ddcf6d465928303984c38a90a3
Victor Gonzalez <dazedbutamused@gmail.com>
2020-10-03 21:10:27 -0700

    eslint fix

--- client/src/components/User/Guest/List.jsx
+++ client/src/components/User/Guest/List.jsx
@@ -34,11 +34,6 @@ class List extends React.Component {
             {guest.name}
           </ListGroup.Item>
         ))}
-        {/* <ListGroup.Item>Luna</ListGroup.Item> */}
-        {/* <ListGroup.Item>Usagi</ListGroup.Item>
-      <ListGroup.Item>Sailor Senshi</ListGroup.Item>
-      <ListGroup.Item>Tuxedo Kame</ListGroup.Item>
-      <ListGroup.Item>Yaas Queen Beryl</ListGroup.Item> */}
       </ListGroup>
     );
   }
